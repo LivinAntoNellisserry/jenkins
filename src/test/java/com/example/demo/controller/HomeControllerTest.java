2fa147be7f02064bc0379348aa5a71e99fd7c8a3
LivinAntoNellisserry <livin.nellisserry@gmail.com>
2021-10-12 18:29:59 +0530

    initial commit

---
+++ src/test/java/com/example/demo/controller/HomeControllerTest.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class HomeControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void homePageTest() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+	}
+
+}
